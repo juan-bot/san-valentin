38dfc53dbaf9439d937c24d9c53a0fe018e3ff3b
Juan Pablo Anuard Vazquez Guzman <56168229+juan-bot@users.noreply.github.com>
2025-02-16 16:41:58 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const envelope = document.querySelector('.envelope-wrapper');
 const letter = document.querySelector('.letter');
 
-
+const reg = document.getElementById("regaloid")
 document.addEventListener('click', (e) => {
     if (
         e.target.matches(".envelope") || 
@@ -10,6 +10,7 @@ document.addEventListener('click', (e) => {
         e.target.matches(".heart")
     ) {
         envelope.classList.toggle('flap');
+        
     } else if (e.target.matches(".envelope *")) {
         if (!letter.classList.contains('opened')) {
             letter.classList.add("letter-opening");
@@ -23,6 +24,7 @@ document.addEventListener('click', (e) => {
             letter.classList.add('closing-letter')
             envelope.classList.remove("disable-envelope")
             letter.classList.remove('opened')
+            reg.style.display = "block"
             setTimeout(() => {
                 letter.classList.remove('closing-letter');
                 letter.classList.remove('opened');
